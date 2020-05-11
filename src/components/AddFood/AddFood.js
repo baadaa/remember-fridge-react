0d6a05dc11c6791f8b650998cc40c876efa69f68
Bumhan Yu <bumhan.yu@gmail.com>
2020-05-11 13:29:13 -0400

    convert AddFoo and Footer components into styled components && remove scss modules for them

--- src/components/AddFood/AddFood.js
+++ src/components/AddFood/AddFood.js
@@ -1,16 +1,53 @@
 import React from "react";
-import css from "./AddFood.module.scss";
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 100%;
+  max-width: 360px;
+  display: flex;
+  margin: 0 auto 30px;
+  background: #fff;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  outline: none;
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.3);
+  padding: 20px 0;
+  border-radius: 20px;
+
+  .plus {
+    width: 30px;
+    height: 30px;
+    margin-right: 10px;
+    font-size: 30px;
+    font-weight: 400;
+    border: 1px solid #aaa;
+    color: #aaa;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 40px;
+    flex-grow: 0;
+  }
+
+  .text {
+    color: #aaa;
+    text-align: left;
+    flex-basis: 120px;
+    line-height: 1.2;
+  }
+`;
 
 const AddFood = props => (
   <div style={{ width: "100%", margin: "0 auto" }}>
-    <button className={css.btn} onClick={props.click}>
-      <span className={css.plus}>+</span>
-      <span className={css.text}>
+    <Button onClick={props.click}>
+      <span className="plus">+</span>
+      <span className="text">
         Add an item
         <br />
         to <span style={{ textTransform: "capitalize" }}>{props.category}</span>
       </span>
-    </button>
+    </Button>
   </div>
 );
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,41 @@
 import React from "react";
-import css from "./Footer.module.scss";
+import styled from "styled-components";
 
 const year = new Date().getFullYear();
 
-const Footer = () => (
-  <footer className={css.foot}>
+const Footer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 1.5rem;
+  font-size: 0.8rem;
+  line-height: 1.5rem;
+  text-align: center;
+  background: #999;
+  color: #fff;
+  box-shadow: 0px -2px 3px rgba(0, 0, 0, 0.4);
+
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
+
+  .logoutBtn {
+    position: fixed;
+    right: 1.5rem;
+    font-size: 0.5rem;
+    color: #bbb;
+    cursor: pointer;
+  }
+`;
+
+const FooterBlock = () => (
+  <Footer>
     Copyright &copy;{year} Bumhan Yu
-    <a href="/" className={css.logoutBtn}>
+    <a href="/" className="logoutBtn">
       Logout
     </a>
-  </footer>
+  </Footer>
 );
-export default Footer;
+export default FooterBlock;
