170a63349b6bd024939d6d74e1318645d7882359
Bumhan Yu <bumhan.yu@gmail.com>
2020-05-12 00:48:10 -0400

    convert FoodEditor to styled-components && remove related scss module

--- src/components/FoodEditor/FoodEditor.js
+++ src/components/FoodEditor/FoodEditor.js
@@ -5,7 +5,193 @@ import blank from "../../img/_.png";
 // import closeIcon from "../img/close.svg";
 // import expireIcon from "../img/expires.svg";
 // import ColorPalette from "./ColorPalette";
-import css from "./FoodEditor.module.scss";
+import styled from "styled-components";
+import photoPrompt from "../../img/add-photo.svg";
+
+const fieldsWidth = 223;
+const EditorOverlay = styled.div`
+  position: fixed;
+  z-index: 3;
+  background: rgba(0, 0, 0, 0.9);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  color: #fff;
+  align-items: center;
+  justify-content: center;
+  transition: transform 0.3s, opacity 0.3s;
+  transform: ${props => (props.isOpen ? "translateX(0)" : "translateX(100vw)")};
+  opacity: ${props => (props.isOpen ? 1 : 0)};
+  & > div {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    position: fixed;
+    top: 0;
+    left: 0;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const EditorOverlayWrapper = ({ isOpen, children }) => (
+  <EditorOverlay isOpen={isOpen}>
+    <div>{children}</div>
+  </EditorOverlay>
+);
+
+const PhotoInput = styled.div`
+  background-position: center;
+  background-repeat: no-repeat;
+  border-radius: 10px;
+  margin-right: 10px;
+  width: ${fieldsWidth}px;
+  height: ${fieldsWidth}px;
+  background-color: #fff;
+  background-image: url(${photoPrompt});
+  background-size: inherit;
+  cursor: pointer;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: ${fieldsWidth}px;
+    height: ${fieldsWidth}px;
+    object-fit: cover;
+  }
+  label {
+    cursor: pointer;
+    height: ${fieldsWidth}px;
+
+    input[type="file"] {
+      position: fixed;
+      top: -1000px;
+    }
+  }
+  @media screen and (max-width: 456px) {
+    margin-right: 0;
+    margin-bottom: 15px;
+  }
+`;
+const PhotoBlock = ({ img, takePhoto }) => (
+  <PhotoInput>
+    <label>
+      <input
+        type="file"
+        id="img"
+        name="img"
+        accept="image/*"
+        placeholder=""
+        onChange={takePhoto}
+      />
+      <img src={img === "" ? blank : img} alt="" />
+    </label>
+  </PhotoInput>
+);
+
+const Form = styled.form`
+  width: 600px;
+  overflow-y: auto;
+  text-align: center;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  h2 {
+    width: 100%;
+    margin: 0;
+    margin-bottom: 0.6em;
+  }
+  @media screen and (max-width: 456px) {
+    flex-direction: column;
+  }
+`;
+
+const EditorForm = ({ editorMode, children }) => (
+  <Form onSubmit={e => e.preventDefault()}>
+    <h2>{editorMode} an item</h2>
+    {children}
+  </Form>
+);
+
+const Fields = styled.div`
+  text-align: left;
+  width: ${fieldsWidth}px;
+`;
+
+const FieldItem = styled.div`
+  width: ${fieldsWidth}px;
+  height: 47px;
+  box-sizing: border-box;
+  border-radius: 7px;
+  font-size: 0.75rem;
+  line-height: 1rem;
+  background: #fff;
+  padding-top: 0.2rem;
+  padding-left: 1rem;
+  margin-bottom: 0.7rem;
+  color: #999;
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+  .req {
+    color: red;
+  }
+  input {
+    margin-top: -1.2rem;
+    margin-left: -1rem;
+    box-sizing: border-box;
+    background: rgba(0, 0, 0, 0);
+    width: 195px;
+    height: 47px;
+    padding-top: 1.1rem;
+    padding-left: 1rem;
+    border: 0;
+    color: #000;
+    border-radius: 7px;
+    font-size: 1rem;
+  }
+`;
+
+const TextField = ({ labelText, id, value, editField, isRequired }) => (
+  <FieldItem>
+    <label htmlFor={id}>{labelText}</label>
+    <span className="req" style={{ display: !isRequired ? "none" : "" }}>
+      *
+    </span>
+    <input
+      type="text"
+      id={id}
+      name={id}
+      placeholder={labelText}
+      onChange={editField}
+      value={value}
+    />
+  </FieldItem>
+);
+
+const DateField = ({ labelText, id, value, isRequired, ref }) => (
+  <FieldItem>
+    <label htmlFor={id}>{labelText}</label>
+    <span className="req" style={{ display: !isRequired ? "none" : "" }}>
+      *
+    </span>
+    <input
+      className="flatpickr-input form-control input"
+      placeholder={labelText}
+      type="text"
+      id={id}
+      name={id}
+      value={value}
+      readOnly="readonly"
+      ref={ref}
+    />
+  </FieldItem>
+);
 
 class FoodEditor extends React.Component {
   addDatePicker = React.createRef();
@@ -31,132 +217,120 @@ class FoodEditor extends React.Component {
     });
   }
   render() {
-    const visible = this.props.open === false ? "" : css.open;
     const {
-      img = "",
-      quantity = "",
-      name = "",
-      added = "",
-      expires = ""
-    } = this.props.currentItem;
+      currentItem: {
+        img = "",
+        quantity = "",
+        name = "",
+        added = "",
+        expires = ""
+      },
+      open: isOpen,
+      editorMode,
+      takePhoto,
+      editField,
+      saveChanges,
+      addNewItem
+    } = this.props;
+
     return (
-      <div className={`${css.editorView} ${visible}`}>
-        <div className={css.editorViewInner}>
-          <form className={css.editor} onSubmit={e => e.preventDefault()}>
-            <h2 onClick={this.closeEditor}>{this.props.editorMode} an item</h2>
-            <div className={css.photo}>
-              <label className={css.photoFile}>
-                <input
-                  type="file"
-                  id="img"
-                  name="img"
-                  accept="image/*"
-                  placeholder=""
-                  onChange={this.props.takePhoto}
-                />
-                <img src={img === "" ? blank : img} alt="" />
+      <EditorOverlayWrapper isOpen={isOpen}>
+        <EditorForm editorMode={editorMode}>
+          <PhotoBlock img={img} takePhoto={takePhoto} />
+          <Fields>
+            <TextField
+              id="name"
+              labelText="Item name"
+              editField={editField}
+              value={name}
+              isRequired={true}
+            />
+            <TextField
+              id="quantity"
+              labelText="Quantity"
+              editField={editField}
+              value={quantity}
+            />
+            {/* <DateField
+              id="added"
+              labelText="Added date"
+              isRequired={true}
+              value={added}
+              ref={this.addDatePicker}
+            /> */}
+            <FieldItem>
+              <label htmlFor="added">
+                Added Date<span className="req">*</span>
               </label>
-            </div>
-            <div className={css.fields}>
-              <div className={css.fieldLabel}>
-                <label htmlFor="name">
-                  Item name<span className="req">*</span>
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Item name"
-                  style={{ boxShadow: "none" }}
-                  value={name}
-                  onChange={this.props.editField}
-                />
-              </div>
-              <div className={css.fieldLabel}>
-                <label htmlFor="quantity">Quantity</label>
-                <input
-                  type="text"
-                  id="quantity"
-                  name="quantity"
-                  placeholder="Quantity"
-                  onChange={this.props.editField}
-                  value={quantity}
-                />
-              </div>
-              <div className={css.fieldLabel} style={{ boxShadow: "none" }}>
-                <label htmlFor="added">
-                  Added Date<span className="req">*</span>
-                </label>
-                <input
-                  className="flatpickr-input form-control input"
-                  placeholder="Added date"
-                  type="text"
-                  id="added"
-                  name="added"
-                  value={added}
-                  readOnly="readonly"
-                  ref={this.addDatePicker}
-                />
-              </div>
-              <div className={css.fieldLabel} style={{ boxShadow: "none" }}>
-                <label htmlFor="expires">Expiration Date (optional)</label>
-                <input
-                  className="flatpickr-input form-control input"
-                  placeholder="Expiring date"
-                  type="text"
-                  id="expires"
-                  name="expires"
-                  value={expires}
-                  readOnly="readonly"
-                  ref={this.expDatePicker}
-                />
-              </div>
-            </div>
-            <div
-              style={{
-                display: this.props.editorMode === "Edit" ? "block" : "none"
-              }}
+              <input
+                className="flatpickr-input form-control input"
+                placeholder="Added date"
+                type="text"
+                id="added"
+                name="added"
+                value={added}
+                readOnly="readonly"
+                ref={this.addDatePicker}
+              />
+            </FieldItem>
+            <FieldItem>
+              <label htmlFor="expires">Expiration Date (optional)</label>
+              <input
+                className="flatpickr-input form-control input"
+                placeholder="Expiring date"
+                type="text"
+                id="expires"
+                name="expires"
+                value={expires}
+                readOnly="readonly"
+                ref={this.expDatePicker}
+              />
+            </FieldItem>
+          </Fields>
+          <div
+            style={{
+              display: editorMode === "Edit" ? "block" : "none"
+            }}
+          >
+            <button
+              style={{ background: "#333", color: "#FFF" }}
+              onClick={saveChanges}
             >
-              <button
-                style={{ background: "#333", color: "#FFF" }}
-                onClick={this.props.saveChanges}
-              >
-                Save
-              </button>
-              <button
-                style={{ background: "#333", color: "#FFF" }}
-                onClick={this.deleteItemFromEditor}
-              >
-                Remove
-              </button>
-              <button
-                style={{ background: "#333", color: "#FFF" }}
-                onClick={this.closeEditor}
-              >
-                Cancel
-              </button>
-            </div>
-            <div
-              style={{
-                display: this.props.editorMode === "Add" ? "block" : "none"
-              }}
+              Save
+            </button>
+            <button
+              style={{ background: "#333", color: "#FFF" }}
+              onClick={this.deleteItemFromEditor}
             >
-              <button
-                style={{ background: "#333", color: "#FFF" }}
-                onClick={this.props.addNewItem}
-              >
-                Add item
-              </button>
-              <button
-                style={{ background: "#333", color: "#FFF" }}
-                onClick={this.closeEditor}
-              >
-                Cancel
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+              Remove
+            </button>
+            <button
+              style={{ background: "#333", color: "#FFF" }}
+              onClick={this.closeEditor}
+            >
+              Cancel
+            </button>
+          </div>
+          <div
+            style={{
+              display: editorMode === "Add" ? "block" : "none"
+            }}
+          >
+            <button
+              style={{ background: "#333", color: "#FFF" }}
+              onClick={addNewItem}
+            >
+              Add item
+            </button>
+            <button
+              style={{ background: "#333", color: "#FFF" }}
+              onClick={this.closeEditor}
+            >
+              Cancel
+            </button>
+          </div>
+        </EditorForm>
+      </EditorOverlayWrapper>
     );
   }
 }
