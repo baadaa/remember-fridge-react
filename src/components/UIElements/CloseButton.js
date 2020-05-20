2dc4dd88516cdc310d75f43f28de86378a0f7867
Bumhan Yu <bumhan.yu@gmail.com>
2020-05-20 09:34:27 -0400

    remove closebutton.js

--- src/components/UIElements/CloseButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import closeBlack from "../../img/close-black.svg";
-import closeWhite from "../../img/close-white.svg";
-const CloseButton = ({ isDark }) =>
-  isDark ? <img src={closeWhite} alt="" /> : <img src={closeBlack} alt="" />;
-
-export default CloseButton;
